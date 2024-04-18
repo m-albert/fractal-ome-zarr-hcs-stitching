6a0ee0697d861e7a96e37f3a3a6edbf8ed0adf62
Marvin Albert <marvin.albert@gmail.com>
2024-04-18 16:07:25 +0200

    Renamed task in task list and updated manifest

--- src/fractal_ome_zarr_hcs_stitching/dev/task_list.py
+++ src/fractal_ome_zarr_hcs_stitching/dev/task_list.py
@@ -2,7 +2,7 @@ from fractal_tasks_core.dev.task_models import ParallelTask
 
 TASK_LIST = [
     ParallelTask(
-        name="Thresholding Task",
+        name="Stitching Task",
         executable="stitching_task.py",
         meta={"cpus_per_task": 1, "mem": 4000},
     ),
