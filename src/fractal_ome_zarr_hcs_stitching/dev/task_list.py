ef398c54b893e76284a65561804e7fe4f12b21ea
jluethi <joel.luethi@uzh.ch>
2024-07-17 11:52:03 +0200

    Update manifest

--- src/fractal_ome_zarr_hcs_stitching/dev/task_list.py
+++ src/fractal_ome_zarr_hcs_stitching/dev/task_list.py
@@ -3,6 +3,8 @@ from fractal_tasks_core.dev.task_models import ParallelTask
 TASK_LIST = [
     ParallelTask(
         name="Stitching Task",
+        input_types={"stitched": False},
+        output_types={"stitched": True},
         executable="stitching_task.py",
         meta={"cpus_per_task": 1, "mem": 4000},
     ),
