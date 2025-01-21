ed74516330f9432f2c300beacb800eebe95a1ed4
jluethi <joel.luethi@uzh.ch>
2025-01-21 10:42:47 +0100

    Add dimension separator for full-res writing of Zarr

--- src/fractal_ome_zarr_hcs_stitching/stitching_task.py
+++ src/fractal_ome_zarr_hcs_stitching/stitching_task.py
@@ -237,6 +237,7 @@ def stitching_task(
         chunks=fused_da.chunksize,
         dtype=fused_da.dtype,
         write_empty_chunks=False,
+        dimension_separator="/",
         fill_value=0,
         mode="w",
     )
